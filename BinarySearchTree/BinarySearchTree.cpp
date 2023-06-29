d8cf4bad04762152f11a726b0aa30cc9531b5db4
bimbim1214 <bimoadityapangestu@gmail.com>
2023-06-29 21:08:08 +0700

    untuk mencari node yang akan menjadi parent dari node yang akan dimasukkan

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -37,5 +37,6 @@ public:
 
 		Node* parent = NULL;
 		Node* currentNode = NULL;
+		search(element, parent, currentNode); //locate the node which will be the parent of the node to be inserted
 	}
 };
